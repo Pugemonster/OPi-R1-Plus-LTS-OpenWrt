b5b2895456254d1e4a4a990ef86d9937f90bfb4f
Pugemon <64846484+Pugemon@users.noreply.github.com>
2023-07-09 02:54:37 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# Copyright (c) 2023 Pugemon
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+# https://github.com/Pugemon/OPi-R1-Plus-LTS-OpenWrt
+# Description: Build OpenWrt using GitHub Actions
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1' package/base-files/files/bin/config_generate
