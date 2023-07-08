7406932d456cecb966012f0dea6f213b5d3d57a6
Pugemon <64846484+Pugemon@users.noreply.github.com>
2023-07-09 02:56:57 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,3 +11,9 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1' package/base-files/files/bin/config_generate
+
+#Download Argon theme and ruAntiBlock
+cd package
+git clone https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/gSpotx2f/ruantiblock_openwrt.git
+cd -
