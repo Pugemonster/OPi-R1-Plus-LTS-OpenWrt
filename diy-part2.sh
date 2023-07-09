402a7d1244f0de4b3f30a9016fcdca5d73aa10ca
Pugemon <64846484+Pugemon@users.noreply.github.com>
2023-07-09 03:38:54 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.0.1' package/base-files/files/bin/config_generate
 
 #Download Argon theme and ruAntiBlock
 cd package
